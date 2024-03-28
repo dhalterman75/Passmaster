d693090787548bf1f387164744c4766ed87bf2bc
dhalterman75 <dhalterman75@gmail.com>
2024-03-28 14:46:19 -0400

    added blank homepage and navbar

--- mern/client/src/components/pages/Main.js
+++ mern/client/src/components/pages/Main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from '../pages/Home';
 import Signup from '../pages/Signup';
@@ -9,13 +9,16 @@ import Passwords from '../pages/Passwords';
 
 const Main = () => {
   return (
-    <Switch> {/* The Switch decides which component to show based on the current URL.*/}
-      <Route exact path='/' element={<Home/>}></Route>
-      <Route exact path='/signup' element={<Signup/>}></Route>
-      <Route exact path='/login' element={<Login/>}></Route>
-      <Route exact path='/account' element={<Account/>}></Route>
-      <Route exact path='/passwords' element={<Passwords/>}></Route>
-    </Switch>
+    <Router>
+      <Routes> {/* The Switch decides which component to show based on the current URL.*/}
+        <Route exact path='/' element={<Home/>}></Route>
+        <Route exact path='/signup' element={<Signup/>}></Route>
+        <Route exact path='/login' element={<Login/>}></Route>
+        <Route exact path='/account' element={<Account/>}></Route>
+        <Route exact path='/passwords' element={<Passwords/>}></Route>
+      </Routes>
+    </Router>
+    
   );
 }
 
