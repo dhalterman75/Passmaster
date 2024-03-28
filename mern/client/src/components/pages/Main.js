802ecd43d598e62a12f582986a3f5b06fe554111
dhalterman75 <dhalterman75@gmail.com>
2024-03-28 13:25:15 -0400

    working on routes for each site page

---
+++ mern/client/src/components/pages/Main.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from '../pages/Home';
+import Signup from '../pages/Signup';
+import Login from '../pages/Login';
+import Account from '../pages/Account';
+import Passwords from '../pages/Passwords';
+
+const Main = () => {
+  return (
+    <Switch> {/* The Switch decides which component to show based on the current URL.*/}
+      <Route exact path='/' element={<Home/>}></Route>
+      <Route exact path='/signup' element={<Signup/>}></Route>
+      <Route exact path='/login' element={<Login/>}></Route>
+      <Route exact path='/account' element={<Account/>}></Route>
+      <Route exact path='/passwords' element={<Passwords/>}></Route>
+    </Switch>
+  );
+}
+
+export default Main;
--- mern/client/src/index.js
+++ mern/client/src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from 'react-router-dom';
+import ReactDOM from 'react-dom';
 
+/*
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+);
+*/
+
+ReactDOM.render((
+  <BrowserRouter>
+    <App /> {/* the components of the page*/}
+  </BrowserRouter>
+  ), document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
